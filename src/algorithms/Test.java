3930c49e15c86174adf5b98da83fee3a8ccc8839
AndreasHeindorff <heindorff94@gmail.com>
2017-03-23 23:37:14 +0100

    Added get() method and mapping

--- src/algorithms/Test.java
+++ src/algorithms/Test.java
@@ -15,6 +15,7 @@ public class Test
 {
     public static void main(String[] args)
     {
+        /*
         RedBlackBST<String> myTree = new RedBlackBST<String>(Comparator.naturalOrder());
         myTree.insert("Q"); 
         myTree.insert("A");
@@ -22,6 +23,28 @@ public class Test
         myTree.insert("E");
         myTree.insert("D");
         myTree.insert("C");
+                */
+        
+        IMap<Integer, String> myTree = new RedBlackMap<Integer, String>(Comparator.naturalOrder());
+        myTree.put(2, "G");
+        myTree.put(10, "J");
+        myTree.put(4, "A");
+        myTree.put(7, "L");
+        
+        System.out.println(myTree.size());
+        System.out.println(myTree.get(2));
+        myTree.put(1, "I");
+        myTree.put(20, "N");
+        myTree.put(30, "S");
+        
+        System.out.println(myTree.get(20));
+        System.out.println(myTree.size());
+        
+        myTree.put(13, "Y");
+        
+        System.out.println(myTree.size());
+        System.out.println(myTree.get(20));
+       
        
     }
 }
