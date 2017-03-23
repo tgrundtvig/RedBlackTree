f8c393f6f9167b052a22e4d6acd7a8e2b1480e56
KSWorm <kasper@sylvestworm.dk>
2017-03-23 14:35:03 +0100

    feat(MapNode): added map node

--- src/algorithms/map/RedBlackNode.java
+++ src/algorithms/map/RedBlackNode.java
@@ -9,6 +9,61 @@ package algorithms.map;
  *
  * @author kasper
  */
-public class RedBlackNode {
+public class RedBlackNode<K, V> {
+    private K key;
+    private V value;
+    private RedBlackNode<K, V> left;
+    private RedBlackNode<K, V> right;
+    private boolean isRed;
+
+    public RedBlackNode(K key, V value, RedBlackNode<K, V> left, RedBlackNode<K, V> right, boolean isRed) {
+        this.key = key;
+        this.value = value;
+        this.left = left;
+        this.right = right;
+        this.isRed = isRed;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+
+    public RedBlackNode<K, V> getLeft() {
+        return left;
+    }
+
+    public void setLeft(RedBlackNode<K, V> left) {
+        this.left = left;
+    }
+
+    public RedBlackNode<K, V> getRight() {
+        return right;
+    }
+
+    public void setRight(RedBlackNode<K, V> right) {
+        this.right = right;
+    }
+
+    public boolean isIsRed() {
+        return isRed;
+    }
+
+    public void setIsRed(boolean isRed) {
+        this.isRed = isRed;
+    }
+
+    
     
 }
